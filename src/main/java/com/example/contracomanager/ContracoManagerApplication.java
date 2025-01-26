ed9e3de445def857616de3b739a5c69c9b415ac7
Amr <amrabdelhay26@gmail.com>
2025-01-26 15:51:42 +0100

    fix: improve JPA configuration and add error handling

--- src/main/java/com/example/contracomanager/ContracoManagerApplication.java
+++ src/main/java/com/example/contracomanager/ContracoManagerApplication.java
@@ -3,10 +3,12 @@ package com.example.contracomanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.contracomanager")
+@EnableJpaRepositories(basePackages = "com.example.contracomanager.repository")
 @EnableRetry
 public class ContracoManagerApplication {
 
--- src/main/java/com/example/contracomanager/config/DatabaseConfig.java
+++ src/main/java/com/example/contracomanager/config/DatabaseConfig.java
@@ -9,12 +9,16 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@EntityScan(basePackages = "com.example.contracomanager.model")
+@Slf4j
 public class DatabaseConfig {
     
     @Value("${SPRING_DATASOURCE_URL}")
@@ -28,38 +32,59 @@ public class DatabaseConfig {
     
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder
-            .create()
-            .url(url)
-            .username(username)
-            .password(password)
-            .driverClassName("org.postgresql.Driver")
-            .build();
+        log.info("Configuring DataSource with URL: {}", url);
+        try {
+            return DataSourceBuilder
+                .create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
+        } catch (Exception e) {
+            log.error("Failed to create DataSource: {}", e.getMessage(), e);
+            throw e;
+        }
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.example.contracomanager.model");
-        
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        properties.setProperty("hibernate.show_sql", "false");
-        properties.setProperty("hibernate.format_sql", "true");
-        em.setJpaProperties(properties);
-        
-        return em;
+        log.info("Configuring EntityManagerFactory");
+        try {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource());
+            em.setPackagesToScan("com.example.contracomanager.model");
+            
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            vendorAdapter.setGenerateDdl(true);
+            vendorAdapter.setShowSql(true);
+            em.setJpaVendorAdapter(vendorAdapter);
+            
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "update");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            properties.setProperty("hibernate.show_sql", "true");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.temp.use_jdbc_metadata_defaults", "false");
+            em.setJpaProperties(properties);
+            
+            return em;
+        } catch (Exception e) {
+            log.error("Failed to create EntityManagerFactory: {}", e.getMessage(), e);
+            throw e;
+        }
     }
 
     @Bean
     public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
+        log.info("Configuring TransactionManager");
+        try {
+            JpaTransactionManager transactionManager = new JpaTransactionManager();
+            transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+            return transactionManager;
+        } catch (Exception e) {
+            log.error("Failed to create TransactionManager: {}", e.getMessage(), e);
+            throw e;
+        }
     }
 } 

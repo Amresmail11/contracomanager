cb0dfca1444d7e17df79694533044fbe7da5c2fe
Amr <amrabdelhay26@gmail.com>
2025-01-27 22:53:24 +0100

    Switch to using service-account.json file directly

--- src/main/java/com/example/contracomanager/config/GoogleDriveConfig.java
+++ src/main/java/com/example/contracomanager/config/GoogleDriveConfig.java
@@ -7,12 +7,11 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.io.ByteArrayInputStream;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 
 @Configuration
@@ -20,14 +19,11 @@ public class GoogleDriveConfig {
     private static final String APPLICATION_NAME = "ContracoManager";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    @Value("${google.service.account.credentials}")
-    private String serviceAccountCredentials;
-
     @Bean
     public Drive driveService() throws IOException, GeneralSecurityException {
         HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         GoogleCredentials credentials = GoogleCredentials
-            .fromStream(new ByteArrayInputStream(serviceAccountCredentials.getBytes(StandardCharsets.UTF_8)))
+            .fromStream(new ClassPathResource("service-account.json").getInputStream())
             .createScoped("https://www.googleapis.com/auth/drive.file");
 
         return new Drive.Builder(httpTransport, JSON_FACTORY, new HttpCredentialsAdapter(credentials))
